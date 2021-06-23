020823d79ffbfb20a3380fe2540447ff3859ce03
alexelliott22 <81639053+alexelliott22@users.noreply.github.com>
2021-06-23 10:33:03 -0400

    add animal fetch request

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const path = require('path');
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -95,8 +96,24 @@ app.post('/api/animals', (req, res) => {
       const animal = createNewAnimal(req.body, animals);
       res.json(animal);
   }
-});
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+})
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'))
+})
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'))
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+})
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
-});
+})
